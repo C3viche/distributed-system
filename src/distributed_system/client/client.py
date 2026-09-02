cc22b00b1d3438404d027de0c5d937a214c1d299
C3viche <28328104+C3viche@users.noreply.github.com>
2026-09-02 16:57:04 -0400

    client implementation within class

--- src/distributed_system/client/client.py
+++ src/distributed_system/client/client.py
@@ -1,11 +1,34 @@
 import socket
 
-from distributed_system.config import BUFFER_SIZE, DEFAULT_HOST, DEFAULT_PORT
+from distributed_system.config import BUFFER_SIZE, ENCODING
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((DEFAULT_HOST, DEFAULT_PORT)) # connect to server
-    s.sendall(b"Hello World") # send it a message
-    data = s.recv(BUFFER_SIZE) # recieve the response
 
-print(f"Received {data!r}")
+class SocketClient:
+    def __init__(self, buf_size: int = BUFFER_SIZE):
+        self.buf_size: int = buf_size
+        self.s: socket.socket | None = None # declare socket placeholder
 
+    def connect(self, host: str, port: int):
+        # Actually initialize the socket here
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((host, port))
+
+    def send(self, msg: str) -> str:
+        # Check if socket is actually initialized from connect first
+        if not self.s:
+            raise RuntimeError("Client is not connected to a server! Call connect() first.")
+        self.s.sendall(msg.encode(ENCODING)) # send message
+        
+        # TODO: Make separate receive function b/c distributed systems aren't always one-to-one request/response. Asynchronous heartbeats or other messages may come from server at any time
+        data = self.s.recv(self.buf_size) # wait for response 
+        print(f"Client: Received {data!r}")
+
+        return data.decode(ENCODING) # return decoded message
+
+        
+    def close(self):
+        # Gracefully tears down the network socket
+        if self.s:
+            self.s.close()
+            self.s = None
+            print("Connection closed cleanly.")
